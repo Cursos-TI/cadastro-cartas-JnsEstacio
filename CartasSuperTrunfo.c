210a6caf96ca1e43bd9d146f227114eec5a487e7
JnsEstacio <202404464059@alunos.estacio.br>
2025-11-16 17:16:20 -0300

    Update CartasSuperTrunfo.c
    Correção, inclusão do scanf.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,36 +8,49 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-    // Área para definição das variáveis para armazenar as propriedades das cidades
-    char estado1 = 'R';
-    char codigo_carta1 [4] = "R01";
-    char nome_cidade1 [25] = "Gramado";
-    int populacao1 = 40134;
-    float area1 = 237019.00;
-    float pib1 = 2.66; // %
-    int pontos_turisticos1 = 40;
+    // Área para definição das variáveis da carta 1.
+    
+    char estado1, codigo_carta1 [4], nome_cidade1 [25]; 
+    int populacao1, pontos_turisticos1;
+    float area1, pib1;
+    
+    // Área para definição das variáveis da carta 2.
+    
+    char estado2, codigo_carta2 [4], nome_cidade2 [25]; 
+    int populacao2, pontos_turisticos2;
+    float area2, pib2;
 
-    char estado2 = 'P';
-    char codigo_carta2 [4] = "P02";
-    char nome_cidade2 [25] = "Campina Grande";
-    int populacao2 = 400000;
-    float area2 = 593026.00;
-    float pib2 = 80.10; // %
-    int pontos_turisticos2 = 15;
+    // Área para coleta e exibição dos dados das cartas.
+    //Também, reconhece a acentuação nas palavras.
+    
+    setlocale(LC_ALL, ""); 
 
-    // Área para exibição dos dados das cartas.
-    setlocale(LC_ALL, ""); //Também reconhece a acentuação nas palavras.
-
+    // Área para coleta dos dados e impressão na tela.
+    
     printf("SUPER TRUNFO - CARTA I \n");
-    printf("Letra do Estado: %c\n", estado1);
-    printf("Codigo da Carta: %s\n", codigo_carta1);
-    printf("Nome da Cidade: %s\n", nome_cidade1);
-    printf("População: %d\n", populacao1);
-    printf("Área: %.2f km²\n", area1);
-    printf("Pib: %.1f % bilhões de reais\n", pib1);
-    printf("Pontos (Regiões) Turísticos: %d\n\n\n", pontos_turisticos1);
+    
+    printf("Digite a letra do Estado: \n");
+    scanf("%c", &estado1);
+    
+    printf("Digite o codigo da Carta (Ex: A01):\n");
+    scanf ("%s", &codigo_carta1);
+    
+    printf("digite o nome da Cidade: \n"); 
+    scanf("%s", &nome_cidade1);
+    
+    printf("Digite a população da cidade: \n"); 
+    scanf("%d", &populacao1);
+    
+    printf("Digite a área em km²: \n"); 
+    scanf("%f", &area1);
+    
+    printf("Digite o pib: \n");
+    scanf("%f", pib1);
+    
+    printf("Pontos (Regiões) Turísticos: \n\n\n"); 
+    scanf("%d", pontos_turisticos1);
 
-
+/*
     printf("SUPER TRUNFO - CARTA II \n");
     printf("Letra do Estado: %c\n", estado2);
     printf("Codigo da Carta: %s\n", codigo_carta2);
@@ -46,7 +59,7 @@ int main() {
     printf("Área: %.2f km²\n", area2);
     printf("Pib: %.1f % bilhões de reais\n", pib2);
     printf("Pontos (Regiões) Turísticos: %d\n", pontos_turisticos2);
-
+*/
   
 
 return 0;
